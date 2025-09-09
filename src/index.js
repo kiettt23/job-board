352fa61ec0ea3e94a9a3b1f816c1131671c92f4a
kiettt23 <kiettt23@gmail.com>
2025-09-09 22:05:09 +0700

    feat(theme): wrap app with ThemeProvider and CssBaseline

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { getTheme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={getTheme("light")}>
+    <CssBaseline />
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );

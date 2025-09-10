1c7e1e57dbf4f4072d42b9ad4c5951858fabd657
kiettt23 <kiettt23@gmail.com>
2025-09-10 09:38:40 +0700

    fix(structure): resolve invalid element error by renaming theme index file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { getTheme } from "./theme";
+import { getTheme } from "./theme/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

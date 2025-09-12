01d4ef023e91bb4e818b11dfed910706cc8ea951
kiettt23 <kiettt23@gmail.com>
2025-09-12 16:32:53 +0700

    chore: debug baseURL log

--- src/app/apiService.js
+++ src/app/apiService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import config from "./config";
 
+console.log("API_BASE from env:", process.env.REACT_APP_API_BASE);
 const api = axios.create(config);
+console.log("Axios instance baseURL:", api.defaults.baseURL);
 
 export default api;
